51ee257daa33d2119d2a5a196cc6734dc5d2b42d
ratoz <herlandro.1999@students.amikom.ac.id>
2019-12-20 09:32:56 +0700

    Pam Air

--- app/src/main/java/com/pamair/baranghilang/MainActivity.java
+++ app/src/main/java/com/pamair/baranghilang/MainActivity.java
@@ -1,14 +1,74 @@
 package com.pamair.baranghilang;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    FirebaseDatabase mDatabase;
+    DatabaseReference Users;
+
+    EditText edtUsername,edtPassword;
+    Button btnSignIn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDatabase = FirebaseDatabase.getInstance();
+        Users = mDatabase.getReference("Users");
+        btnSignIn = findViewById(R.id.btn_login);
+
+        edtUsername = findViewById(R.id.box_Name) ;
+        edtPassword = findViewById(R.id.box_Password) ;
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Login process",  Toast.LENGTH_SHORT).show();
+                login(edtUsername.getText().toString(),edtPassword.getText().toString());
+            }
+        });
+    }
+
+    private void login(final String username, final String password) {
+        Users.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange( DataSnapshot dataSnapshot) {
+                if(dataSnapshot.child(username).exists()){
+                    if (!username.isEmpty()){
+                        User login = dataSnapshot.child(username).getValue(User.class);
+                        if(login.getPassword().equals(password)){
+                            Toast.makeText(MainActivity.this, "Success Login",  Toast.LENGTH_SHORT).show();
+
+                        }
+                        else Toast.makeText(MainActivity.this, "Password Wrong",  Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Username does not recognized!",  Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
