26427a1b4b939aa2ccaa7971509a67f8968d2990
ratoz <herlandro.1999@students.amikom.ac.id>
2019-12-27 13:24:57 +0700

    Pam Air v.0.0.1 build

--- app/src/main/java/Model/User.java
+++ app/src/main/java/Model/User.java
@@ -1,4 +1,4 @@
-package com.pamair.baranghilang;
+package Model;
 
 public class User {
     private String username;
--- app/src/main/java/com/pamair/baranghilang/MainActivity.java
+++ app/src/main/java/com/pamair/baranghilang/MainActivity.java
@@ -1,74 +1,59 @@
 package com.pamair.baranghilang;
 
+import Model.User;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.errorprone.annotations.Var;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 
 public class MainActivity extends AppCompatActivity {
 
-
-    FirebaseDatabase mDatabase;
-    DatabaseReference Users;
-
-    EditText edtUsername,edtPassword;
-    Button btnSignIn;
-
+    final FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mDatabase = FirebaseDatabase.getInstance();
-        Users = mDatabase.getReference("Users");
-        btnSignIn = findViewById(R.id.btn_login);
+        Button login = findViewById(R.id.loginclick);
+        final EditText username = findViewById(R.id.txtbox_username);
+        EditText password = findViewById(R.id.txtbox_password);
 
-        edtUsername = findViewById(R.id.box_Name) ;
-        edtPassword = findViewById(R.id.box_Password) ;
-        btnSignIn.setOnClickListener(new View.OnClickListener() {
+        login.setOnClickListener(new View.OnClickListener(){
+
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Login process",  Toast.LENGTH_SHORT).show();
-                login(edtUsername.getText().toString(),edtPassword.getText().toString());
-            }
-        });
-    }
 
-    private void login(final String username, final String password) {
-        Users.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange( DataSnapshot dataSnapshot) {
-                if(dataSnapshot.child(username).exists()){
-                    if (!username.isEmpty()){
-                        User login = dataSnapshot.child(username).getValue(User.class);
-                        if(login.getPassword().equals(password)){
-                            Toast.makeText(MainActivity.this, "Success Login",  Toast.LENGTH_SHORT).show();
-
+                DocumentReference loginref = db.collection("Users").document(username.getText().toString());
+                loginref.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()){
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()){
+                                
+                            }
                         }
-                        else Toast.makeText(MainActivity.this, "Password Wrong",  Toast.LENGTH_SHORT).show();
                     }
-
-                }
-                else {
-                    Toast.makeText(MainActivity.this, "Username does not recognized!",  Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
+                });
+            
             }
         });
+
+
     }
 }
