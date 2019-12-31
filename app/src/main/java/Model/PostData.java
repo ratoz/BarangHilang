46dd872877068804889136057bd7becc126db8f9
ridwanal24 <ridwanalakhyar1999@gmail.com>
2019-12-31 10:53:44 +0700

    Nyoba

---
+++ app/src/main/java/Model/PostData.java
@@ -0,0 +1,51 @@
+package Model;
+
+import java.util.Date;
+
+public class PostData {
+    private String idUser;
+    private String title;
+    private String chronology;
+    private String photo;
+    private String dateLost;
+    private String typePost;
+    private String datePost;
+
+    public PostData(String idUser, String title, String chronology, String photo, String dateLost, String typePost, String datePost) {
+        this.idUser = idUser;
+        this.title = title;
+        this.chronology = chronology;
+        this.photo = photo;
+        this.dateLost = dateLost;
+        this.typePost = typePost;
+        this.datePost = datePost;
+    }
+
+    public void setIdUser(String idUser) {
+        this.idUser = idUser;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setChronology(String chronology) {
+        this.chronology = chronology;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public void setDateLost(String dateLost) {
+        this.dateLost = dateLost;
+    }
+
+    public void setTypePost(String typePost) {
+        this.typePost = typePost;
+    }
+
+    public void setDatePost(String datePost) {
+        this.datePost = datePost;
+    }
+}
