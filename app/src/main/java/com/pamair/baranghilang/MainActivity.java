91aee9a10396a9c640d1ec9eb84e9e6e57c33744
ratoz <herlandro.1999@students.amikom.ac.id>
2020-01-01 20:42:05 +0700

    Pam Air v.0.0.2b build

--- app/src/main/java/com/pamair/baranghilang/MainActivity.java
+++ app/src/main/java/com/pamair/baranghilang/MainActivity.java
@@ -1,21 +1,24 @@
 package com.pamair.baranghilang;
 
-import Model.User;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
+
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,25 +29,26 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-
-import org.w3c.dom.Text;
+import com.pamair.baranghilang.references.sessionLogin;
 
 
 public class MainActivity extends AppCompatActivity {
 
     final FirebaseFirestore db = FirebaseFirestore.getInstance();
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        Button login = findViewById(R.id.loginclick);
+        final RelativeLayout windowprogress = findViewById(R.id.windowprogress);
+        final Button login = findViewById(R.id.loginclick);
         final EditText username = findViewById(R.id.txtbox_username);
         final EditText password = findViewById(R.id.txtbox_password);
         final ImageView warningicon = findViewById(R.id.warningicon);
         final TextView warningtext = findViewById(R.id.warningtext);
+
 
         login.setOnClickListener(new View.OnClickListener(){
 
@@ -53,13 +57,17 @@ public class MainActivity extends AppCompatActivity {
                 Boolean textempty= true;
                 if(TextUtils.isEmpty(username.getText().toString())){
                     textempty = false;
-                    username.setError("Tulis nama tolo!");
+                    username.setError("Tulis nama dengan benar!");
                 }
                 if(TextUtils.isEmpty(password.getText().toString())){
                     textempty = false;
-                    password.setError("Tulis password juga tolo!");
+                    password.setError("Tulis password juga dengan benar!");
                 }
                 if (textempty){
+                    getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                            WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+                    windowprogress.setVisibility(View.VISIBLE);
+                   login.setBackgroundResource(R.color.colorBlackTrans);
                 DocumentReference loginref = db.collection("users").document(username.getText().toString());
                 loginref.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                     @Override
@@ -72,15 +80,18 @@ public class MainActivity extends AppCompatActivity {
                                 String pass = document.get("password").toString();
                                 String passtxtbox = password.getText().toString();
                                 if(pass.equals(passtxtbox)){
+                                    DissapearProgress(login,windowprogress);
                                     Intent dashboard = new Intent(MainActivity.this, halamanDefault.class);
                                     startActivity(dashboard);
 
                                 }
                                 else{
+                                    DissapearProgress(login,windowprogress);
                                      validatefail = false;
                                 }
                             }
                             else {
+                                DissapearProgress(login,windowprogress);
                                 Context context = getApplicationContext();
                                 CharSequence text = "Tidak dapat menemukan user! ";
                                 int duration = Toast.LENGTH_SHORT;
@@ -91,11 +102,14 @@ public class MainActivity extends AppCompatActivity {
                             }
 
                             if (validatefail!=true) {
+                                DissapearProgress(login,windowprogress);
                                 warningicon.setImageResource(R.drawable.draw_warning);
                                 warningtext.setText("NIM atau Password salah!");
                             }
                         }
                         else{
+                            DissapearProgress(login,windowprogress);
+
                             Context context = getApplicationContext();
                             CharSequence text = "Anda sedang offline tolong ya ingat ini online tolo";
                             int duration = Toast.LENGTH_SHORT;
@@ -115,6 +129,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+    }
+
+    void DissapearProgress(Button login, RelativeLayout windowprogress){
+        login.setBackground(ContextCompat.getDrawable(this, R.drawable.button));
+        windowprogress.setVisibility(View.GONE);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
 
     }
 }
